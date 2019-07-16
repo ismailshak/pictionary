e0570baf1d80ccde28ae1662acd28e4f9cfdb544
Shak <ismailshak94@Ismails-MacBook-Air.local>
2019-07-16 14:38:46 -0400

    sockets work now, phase 1

--- pictionary-react/src/Canvas/Canvas.js
+++ pictionary-react/src/Canvas/Canvas.js
@@ -1,224 +1,132 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import io from "socket.io-client";
 
 const socket = io('localhost:8080');
 
+
+
 export default class Canvas extends Component {
-    constructor() {
-        super()
-        this.state = {
-            connected: 0,
-            id: sessionStorage.id,
-            windowHeight: window.innerHeight,
-            windowWidth: window.innerWidth,
-            drawing: false,
-            username: ""
-        }
+  constructor() {
+    super()
+    this.state = {
+      username: "",
+      isDrawing: false,
+      ctx: null,
+      currentX: null, 
+      currentY: null,
+    }
 
-        this.canvas = React.createRef();
+    this.canvasRef = React.createRef();
 
-       
-        socket.on('joined', (joined, id) => {
-            sessionStorage.id = id;
-            this.setState({
-                connected: joined
-            })
-        })
-        socket.on('left', left => {
-            this.setState({connected: left})
-        })
+    socket.on('joined', (joined, id) => {
+      // sessionStorage.id = id;
+      // this.setState({
+      //     connected: joined
+      // })
+    })
 
-        socket.on("drawing", data => {
-            let w = window.innerWidth;
-            let h = window.innerHeight;
-      
-            if (!isNaN(data.x0 / w) && !isNaN(data.y0)) {
-                console.log("draw")
-              this.drawLine(
-                data.x0 * w,
-                data.y0 * h,
-                data.x1 * w,
-                data.y1 * h,
-                data.color
-              );
-            }
-          });
-        }
-        // this.socket.on('drawer', id => {
-        //     this.setState({
-        //         drawer: id,
-        //     })
-        // })
-    
+    socket.on('left', left => {
+        // this.setState({connected: left})
+    })
 
-    
-  componentDidMount() {
-    this.setState({
-      canvas: this.canvas.current
-    });
-    this.canvas.current.style.height = window.innerHeight;
-    this.canvas.current.style.width = window.innerWidth;
-
-    this.canvas.current.addEventListener(
-      "mousedown",
-      this.onMouseDown,
-      false
-    );
-    this.canvas.current.addEventListener("mouseup", this.onMouseUp, false);
-    this.canvas.current.addEventListener("mouseout", this.onMouseUp, false);
-    this.canvas.current.addEventListener(
-      "mousemove",
-      this.throttle(this.onMouseMove, 5),
-      false
-    );
-
-    this.canvas.current.addEventListener(
-      "touchstart",
-      this.onMouseDown,
-      false
-    );
-
-    this.canvas.current.addEventListener(
-      "touchmove",
-      this.throttle(this.onTouchMove, 5),
-      false
-    );
-
-    this.canvas.current.addEventListener("touchend", this.onMouseUp, false);
-
-    window.addEventListener("resize", this.onResize);
+    socket.on("drawing", event => {
+        console.log('draw')
+        // let objectEvent = JSON.parse(event)
+        this.drawClient(event.x, event.y)
+        
+      });
   }
-    
-  drawLine = (x0, y0, x1, y1, color, emit, force) => {
-    let context = this.state.canvas.getContext("2d");
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    // context.strokeStyle = color;
-    context.lineWidth = 2;
-    if (force) {
-    	context.lineWidth = 1.75 * (force * (force + 3.75));
-    }
-    context.stroke();
-    context.closePath();
+	
 
-    if (!emit) {
-      return;
-    }
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    this.setState(() => {
-      if (!isNaN(x0 / w)) {
-        socket.emit("drawing", {
-          x0: x0 / w,
-          y0: y0 / h,
-          x1: x1 / w,
-          y1: y1 / h,
-        //   color: color,
-        //   room: this.state.room,
-          force: force
-        });
-
-        // return {
-        //   cleared: false
-        // };
-      }
-    });
-  };
-
-  onMouseDown = e => {
-    this.setState(() => {
-      return {
-        currentX: e.clientX,
-        currentY: e.clientY,
-        drawing: true
-      };
-    });
-  };
-
-  onMouseUp = e => {
-    this.setState(() => {
-      return {
-        drawing: false,
-        currentX: e.clientX,
-        currentY: e.clientY
-      };
-    });
-  };
-
-  onMouseMove = e => {
-    if (!this.state.drawing) {
-      return;
-    }
-
-    this.setState(() => {
-      return {
-        currentX: e.clientX,
-        currentY: e.clientY
-      };
-    }, this.drawLine(this.state.currentX, this.state.currentY, e.clientX, e.clientY, this.state.currentColor, true));
-  };
-
-  onTouchMove = e => {
-    if (!this.state.drawing) {
-      return;
-    }
-    console.log();
-    this.setState(() => {
-      this.drawLine(
-        this.state.currentX,
-        this.state.currentY,
-        e.touches[0].clientX,
-        e.touches[0].clientY,
-        this.state.currentColor,
-        true,
-        e.touches[0].force
-      );
-      return {
-        currentX: e.touches[0].clientX,
-        currentY: e.touches[0].clientY
-      };
-    });
-  };
-
-  onResize = () => {
+	componentDidMount() {
+		const canvas = this.canvasRef.current
     this.setState({
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight
-    });
-  };
+      ctx: canvas.getContext("2d"),
+    })
+  }
 
-  throttle = (callback, delay) => {
-    let previousCall = new Date().getTime();
-    return function() {
-      let time = new Date().getTime();
+	getX = (event) => {
+		if (event.pageX === undefined) {
+			return event.targetTouches[0].pageX - this.canvasRef.current.offsetLeft;
+		}
+		else {
+			return event.pageX - this.canvasRef.current.offsetLeft;
+		}
+	};
 
-      if (time - previousCall >= delay) {
-        previousCall = time;
-        callback.apply(null, arguments);
+	getY = (event) => {
+		if (event.pageY === undefined) {
+			return event.targetTouches[0].pageY - this.canvasRef.current.offsetTop;
+		}
+		else {
+			return event.pageY - this.canvasRef.current.offsetTop;
+		}
+	};
+
+	start = (event) => {
+		//if (activeTool === "pen") {
+      if(true) {
+			this.setState({
+        isDrawing: true,
+      })
+			this.state.ctx.beginPath();
+			this.state.ctx.moveTo(this.getX(event), this.getY(event));
+			event.preventDefault();
+		}
+	};
+
+	draw = (event, emit=false) => {
+		if (this.state.isDrawing) {
+			this.state.ctx.lineTo(this.getX(event), this.getY(event));
+			this.state.ctx.lineWidth = 5;
+			// this.state.ctx.lineJoin = "round";
+      this.state.ctx.stroke();
+      // console.log(emit)
+      if(emit) {
+        console.log('abort')
+        return;
+      } else {
+        console.log('emit')
+        // console.log(typeof(event))
+        // let stringEvent = JSON.stringify(event)
+        // console.log(socket)
+        socket.emit('drawing', {x: this.getX(event), y: this.getY(event)})
       }
-    };
+		}
+		// event.preventDefault();
   };
+  
+  drawClient = (x, y) => {
+      this.state.ctx.lineTo(x, y);
+			this.state.ctx.lineWidth = 5;
+			// this.state.ctx.lineJoin = "round";
+      this.state.ctx.stroke();
+  }
 
-    chooseDrawer = () => {
-        console.log('clicked')
-        this.socket.emit('play')
-    }
+	end = (event) => {
+		if (this.state.isDrawing) {
+			this.state.ctx.stroke();
+			this.state.ctx.closePath();
+			this.setState({
+        isDrawing: false,
+      })
+		}
+		event.preventDefault();
+	};
 
-    render() {
-        return (
-            <div>
-                {/* <h1>{this.state.connected}</h1>
-                <h3>{this.state.id}</h3>
-                <button onClick={this.chooseDrawer}>Choose</button>
-                {this.state.drawer === this.state.id ? <p>You're the drawer</p> : <p>You're the guesser</p>} */}
-                <canvas
-                    height={`${this.state.windowHeight}px`}
-                    width={`${this.state.windowWidth}px`}
-                    ref={this.canvas}
-                    className="canvas"
-                />
-            </div>
-        )
-    }
+  render() {
+
+    return (
+      <canvas
+        width={window.innerWidth}
+        height={window.innerHeight}
+        className="canvas"
+        ref={this.canvasRef}
+        onMouseDown={this.start}
+        onMouseMove={this.draw}
+        onMouseUp={this.end}
+      />
+	  );
+  }
+	
 }
